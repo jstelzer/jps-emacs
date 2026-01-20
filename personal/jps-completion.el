ce6fcb8d2eb3ae35e592134cd5761aaf5ab3cc87
Jason Stelzer <mental@neverlight.com>
2026-01-20 09:43:57 -0500

    Corfu, not company

--- personal/jps-completion.el
+++ personal/jps-completion.el
@@ -15,15 +15,23 @@
 
 (use-package yasnippet :straight t :config (yas-global-mode))
 
-(use-package company
+(use-package corfu
   :straight t
-  :defer 0.1
-  :config
-  (global-company-mode t)
-  (setq company-idle-delay 0.2
-        company-require-match nil
-        company-minimum-prefix-length 3
-        company-frontends '(company-pseudo-tooltip-frontend)))
+  :init
+  (global-corfu-mode 1)
+  :custom
+  (corfu-auto t)                 ; Enable auto completion
+  (corfu-cycle t)                 ; Cycle through candidates
+  (corfu-auto-delay 0.2)          ; Delay before popup
+  (corfu-auto-prefix 2)           ; Min chars before popup
+  (corfu-quit-no-match 'separator)
+  (corfu-preview-current nil))    ; Don't preview current candidate
+
+(use-package cape
+  :straight t
+  :init
+  (add-to-list 'completion-at-point-functions #'cape-file)
+  (add-to-list 'completion-at-point-functions #'cape-dabbrev))
 
 (provide 'jps-completion)
 ;;; jps-completion.el ends here
--- personal/jps-ui.el
+++ personal/jps-ui.el
@@ -24,23 +24,6 @@
         completion-category-defaults nil
         completion-category-overrides '((file (styles . (partial-completion))))))
 
-(use-package corfu
-  :straight t
-  :init
-  (global-corfu-mode 1)
-  :custom
-  (corfu-auto t)                 ; Enable auto completion
-  (corfu-cycle t)                 ; Cycle through candidates
-  (corfu-auto-delay 0.2)          ; Delay before popup
-  (corfu-auto-prefix 2)           ; Min chars before popup
-  (corfu-quit-no-match 'separator)
-  (corfu-preview-current nil))    ; Don't preview current candidate
-
-(use-package cape
-  :straight t
-  :init
-  (add-to-list 'completion-at-point-functions #'cape-file)
-  (add-to-list 'completion-at-point-functions #'cape-dabbrev))
 
 (use-package marginalia :straight t :init (marginalia-mode 1))
 
